e1d60005d8bbde3c827739f7fbb6de58969febde
doliq <diwaoliq@gmail.com>
2023-06-28 08:10:35 +0700

    ep14

--- Data slice/dataslice.go
+++ Data slice/dataslice.go
@@ -25,7 +25,25 @@ func main() {
 	fmt.Println(len(slice1))
 	fmt.Println(cap(slice1))
 
-	months[5] = "16 Juni"
-	fmt.Println(slice1)
+	//months[5] = "16 Juni"
+	//fmt.Println(slice1)
 
+	var slice2 = months[10:]
+	fmt.Println((slice2))
+
+	var slice3 = append(slice2, "Diwa")
+	fmt.Println(slice3)
+
+	newSlice := make([]string, 3, 5)
+
+	newSlice[0] = "Diwa"
+	newSlice[1] = "Qotrun"
+	newSlice[2] = "Nada"
+
+	fmt.Println(newSlice)
+
+	copySlice := make([]string, len(newSlice), cap(months))
+	copy(copySlice, newSlice)
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
 }
